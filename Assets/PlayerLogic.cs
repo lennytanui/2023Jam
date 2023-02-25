32d88f189c64c7c9a052342fbfcb69eb9d05fa85
lennytanui <lennytanui@outlook.com>
2023-02-25 10:34:40 -0600

    more obstacles

---
+++ Assets/PlayerLogic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Paris")){
+            Debug.Log("You Share a heart!");
+        }
+    }
+}
