9d27cd106011403928dbd1d3425ff35d76a17ab6
lennytanui <lennytanui@outlook.com>
2023-02-25 10:10:56 -0600

    Movement for the players Implemented

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,16 +6,58 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float playerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+    string playerTag;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        rb = this.GetComponent<Rigidbody2D>();
+        playerTag = this.gameObject.tag;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        int vertical = 0;
+        int horizontal = 0;
+
+        if(playerTag == "Player 1"){
+            if(Input.GetKey(KeyCode.W)){
+                vertical = 1;
+            }else if(Input.GetKey(KeyCode.S)){
+                vertical = -1;
+            }
+
+            
+            if(Input.GetKey(KeyCode.D)){
+                horizontal = 1;
+            }else if(Input.GetKey(KeyCode.A)){
+                horizontal = -1;
+            }
+
+        } else if(playerTag == "Player 2"){
+            if(Input.GetKey(KeyCode.UpArrow)){
+                vertical = 1;
+            }else if(Input.GetKey(KeyCode.DownArrow)){
+                vertical = -1;
+            }
+
+            
+            if(Input.GetKey(KeyCode.RightArrow)){
+                horizontal = 1;
+            }else if(Input.GetKey(KeyCode.LeftArrow)){
+                horizontal = -1;
+            }  
+        }
+
+        moveDirection = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate(){
+        rb.velocity = new Vector2(moveDirection.x * playerSpeed * Time.fixedDeltaTime, moveDirection.y * playerSpeed * Time.fixedDeltaTime);
     }
 }
