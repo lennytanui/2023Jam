14fb018d1aa3eef2b7363eba99ed0849a6e8367e
lennytanui <lennytanui@outlook.com>
2023-02-24 20:29:46 -0600

    added squares

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
